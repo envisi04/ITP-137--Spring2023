89169cfa7c96273b10ee6d55f99882ace33760f4
Justin D'Ambola <57686479+jdambola-pvcc@users.noreply.github.com>
2023-02-26 02:40:31 +0000

    made FrizzBuzz

--- main.swift
+++ main.swift
@@ -1,1 +1,11 @@
-print("test1")
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("FrizzBuzz")
+    } else if i % 3 == 0 {
+        print("Frizz")
+    } else if i % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
